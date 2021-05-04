b6f7597b618bf55efba48a02814d40f1718a67f0
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-04 18:10:32 +0300

    Initial commit

---
+++ src/test/java/com/github/tripolkaandrey/mintnoteapi/MintnoteApiApplicationTests.java
@@ -0,0 +1,10 @@
+package com.github.tripolkaandrey.mintnoteapi;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintnoteApiApplicationTests {
+
+}
