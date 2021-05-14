db40c6ce748972c82ce2fa794d1c6f576da40a91
unknown <andrii.trypilka@nure.ua>
2021-05-14 15:54:55 +0300

    add endpoint to get note by id

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/controller/NotesController.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/controller/NotesController.java
@@ -2,10 +2,13 @@ package com.github.tripolkaandrey.mintnoteapi.controller;
 
 import com.github.tripolkaandrey.mintnoteapi.entity.Note;
 import com.github.tripolkaandrey.mintnoteapi.repository.NoteRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/notes/")
@@ -19,5 +22,12 @@ public final class NotesController {
     @GetMapping
     public Flux<Note> getNotes() {
         return noteRepository.findAll();
+    }
+
+    @GetMapping("{id}/")
+    public Mono<ResponseEntity<Note>> getNote(@PathVariable String id) {
+        return noteRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
