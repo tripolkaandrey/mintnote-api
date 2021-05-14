d0dcdafb6791b2cd74e4201997fd828c53c9b06b
unknown <andrii.trypilka@nure.ua>
2021-05-14 16:13:17 +0300

    add endpoints for updating note's properties

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/controller/NotesController.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/controller/NotesController.java
@@ -1,14 +1,15 @@
 package com.github.tripolkaandrey.mintnoteapi.controller;
 
 import com.github.tripolkaandrey.mintnoteapi.entity.Note;
+import com.github.tripolkaandrey.mintnoteapi.entity.Tag;
 import com.github.tripolkaandrey.mintnoteapi.repository.NoteRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/notes/")
@@ -28,6 +29,71 @@ public final class NotesController {
     public Mono<ResponseEntity<Note>> getNote(@PathVariable String id) {
         return noteRepository.findById(id)
                 .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}/name/")
+    public Mono<ResponseEntity<Note>> updateName(@PathVariable String id, @RequestBody String name) {
+        return noteRepository.findById(id)
+                .flatMap(
+                        n -> {
+                            n.setName(name);
+                            n.setLastModifiedDate(new Date());
+                            return noteRepository.save(n);
+                        }
+                ).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}/tags/")
+    public Mono<ResponseEntity<Note>> updateTags(@PathVariable String id, @RequestBody List<Tag> tags) {
+        return noteRepository.findById(id)
+                .flatMap(
+                        n -> {
+                            n.setTags(tags);
+                            n.setLastModifiedDate(new Date());
+                            return noteRepository.save(n);
+                        }
+                ).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}/icon/")
+    public Mono<ResponseEntity<Note>> updateIcon(@PathVariable String id, @RequestBody String icon) {
+        return noteRepository.findById(id)
+                .flatMap(
+                        n -> {
+                            n.setIcon(icon);
+                            n.setLastModifiedDate(new Date());
+                            return noteRepository.save(n);
+                        }
+                ).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}/parent/")
+    public Mono<ResponseEntity<Note>> updateParent(@PathVariable String id, @RequestBody String parent) {
+        return noteRepository.findById(id)
+                .flatMap(
+                        n -> {
+                            n.setParent(parent);
+                            n.setLastModifiedDate(new Date());
+                            return noteRepository.save(n);
+                        }
+                ).map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}/content/")
+    public Mono<ResponseEntity<Note>> updateContent(@PathVariable String id, @RequestBody String content) {
+        return noteRepository.findById(id)
+                .flatMap(
+                        n -> {
+                            n.setContent(content);
+                            n.setLastModifiedDate(new Date());
+                            return noteRepository.save(n);
+                        }
+                ).map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
