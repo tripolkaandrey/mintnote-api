b775af9824ca65050a9cd95bd493040cad7eb792
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-24 15:33:16 +0300

    make invalid language code exception final

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/exception/InvalidLanguageCodeException.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/exception/InvalidLanguageCodeException.java
@@ -2,7 +2,7 @@ package com.github.tripolkaandrey.mintnoteapi.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class InvalidLanguageCodeException extends ApiException {
+public final class InvalidLanguageCodeException extends ApiException {
     private static final String ERROR_MESSAGE = "Invalid language code. " +
             "Please check out the list of supported language codes by Google Cloud Translation API";
 
