bd12c9ed107443456721e690ce48b62d0120cf06
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-24 15:33:36 +0300

    make translation service internal error final

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/exception/TranslationServiceInternalError.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/exception/TranslationServiceInternalError.java
@@ -2,7 +2,7 @@ package com.github.tripolkaandrey.mintnoteapi.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class TranslationServiceInternalError extends ApiException {
+public final class TranslationServiceInternalError extends ApiException {
     private static final String ERROR_MESSAGE = "Translation service encountered internal error";
 
     public TranslationServiceInternalError() {
