008d052879da51b8b44d195e5cf0ddf8572e3225
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-24 14:17:43 +0300

    refactor notes controller tests

--- src/test/java/com/github/tripolkaandrey/mintnoteapi/controller/NotesControllerTests.java
+++ src/test/java/com/github/tripolkaandrey/mintnoteapi/controller/NotesControllerTests.java
@@ -2,10 +2,12 @@ package com.github.tripolkaandrey.mintnoteapi.controller;
 
 import com.github.tripolkaandrey.mintnoteapi.entity.Note;
 import com.github.tripolkaandrey.mintnoteapi.entity.Tag;
+import com.github.tripolkaandrey.mintnoteapi.exception.NoteNotFoundException;
 import com.github.tripolkaandrey.mintnoteapi.repository.NoteRepository;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -21,6 +23,7 @@ import java.util.List;
 @SpringBootTest
 @AutoConfigureWebTestClient
 class NotesControllerTests {
+    private static final String NOTES_BASE_URL = "/notes/";
     @Autowired
     private WebTestClient webTestClient;
     @Autowired
@@ -32,168 +35,190 @@ class NotesControllerTests {
     }
 
     @Test
-    void getNotes_ReturnsAllNotes() {
-        final int amountOfNotes = 5;
-        for (int i = 0; i < amountOfNotes; i++) {
-            noteRepository.save(new Note()).block();
-        }
-
-        webTestClient.get().uri("/notes/")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBodyList(Note.class)
-                .hasSize(amountOfNotes);
-    }
-
-    @Test
-    void getNote_ExistingId_Ok() {
-        Note testNote = noteRepository.save(new Note()).block();
-
-        webTestClient.get().uri("/notes/" + testNote.getId() + "/")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Note.class)
-                .consumeWith(response -> {
-                    Note responseNote = response.getResponseBody();
-                    Assertions.assertEquals(testNote.getId(), responseNote.getId());
-                });
-    }
-
-    @Test
-    void getNote_NotExistingId_NotFound() {
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-
-        webTestClient.get().uri("/notes/" + randomString)
-                .exchange()
-                .expectStatus().isNotFound();
-    }
-
-    @Test
-    void updateName_ExistingId_Ok() {
-        Note testNote = noteRepository.save(new Note()).block();
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-
-        webTestClient.put().uri("/notes/" + testNote.getId() + "/name/")
-                .body(Mono.just(randomString), String.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Note.class)
-                .consumeWith(response -> {
-                    Note responseNote = response.getResponseBody();
-                    Assertions.assertEquals(randomString, responseNote.getName());
-                });
-    }
-
-    @Test
-    void updateIcon_ExistingId_Ok() {
-        Note testNote = noteRepository.save(new Note()).block();
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-
-        webTestClient.put().uri("/notes/" + testNote.getId() + "/icon/")
-                .body(Mono.just(randomString), String.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Note.class)
-                .consumeWith(response -> {
-                    Note responseNote = response.getResponseBody();
-                    Assertions.assertEquals(randomString, responseNote.getIcon());
-                });
-    }
-
-    @Test
-    void updateParent_ExistingId_Ok() {
-        Note testNote = noteRepository.save(new Note()).block();
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-
-        webTestClient.put().uri("/notes/" + testNote.getId() + "/parent/")
-                .body(Mono.just(randomString), String.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Note.class)
-                .consumeWith(response -> {
-                    Note responseNote = response.getResponseBody();
-                    Assertions.assertEquals(randomString, responseNote.getParent());
-                });
-    }
-
-    @Test
-    void updateContent_ExistingId_Ok() {
-        Note testNote = noteRepository.save(new Note()).block();
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-
-        webTestClient.put().uri("/notes/" + testNote.getId() + "/content/")
-                .body(Mono.just(randomString), String.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Note.class)
-                .consumeWith(response -> {
-                    Note responseNote = response.getResponseBody();
-                    Assertions.assertEquals(randomString, responseNote.getContent());
-                });
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"name", "parent", "icon", "content"})
-    void updateProperty_NotExistingId_NotFound(String parameterName) {
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-
-        webTestClient.put().uri("/notes/" + randomString + "/" + parameterName)
-                .exchange()
-                .expectStatus().isNotFound();
-    }
-
-    @Test
-    void updateTags_Existing_Ok() {
-        Note testNote = noteRepository.save(new Note()).block();
-        List<Tag> tags = new ArrayList<>();
-        tags.add(new Tag("test", "test"));
-
-        webTestClient.put().uri("/notes/" + testNote.getId() + "/tags/")
-                .body(Mono.just(tags), List.class)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(Note.class).consumeWith(response -> {
-            Note responseNote = response.getResponseBody();
-            Assertions.assertEquals(tags, responseNote.getTags());
-        });
-    }
-
-    @Test
-    void updateTags_NotExisting_NotFound() {
-        String randomString = RandomStringUtils.randomAlphanumeric(10);
-        List<Tag> tags = new ArrayList<>();
-        tags.add(new Tag("test", "test"));
-
-        webTestClient.put().uri("/notes/" + randomString + "/tags")
-                .body(Mono.just(tags), List.class)
-                .exchange()
-                .expectStatus().isNotFound();
-    }
-
-    @Test
-    void createNote_Created() {
+    void Create_Created() {
         Note testNote = new Note();
 
-        webTestClient.post().uri("/notes/")
+        webTestClient.post().uri(NOTES_BASE_URL)
                 .body(Mono.just(testNote), Note.class)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(Note.class)
                 .consumeWith(response -> {
                     Note responseNote = response.getResponseBody();
-                    Assertions.assertNotNull(responseNote.getId());
+                    Assertions.assertNotNull(responseNote);
                 });
     }
 
-    @Test
-    void delete_ExistingId_Ok() {
-        Note note = noteRepository.save(new Note()).block();
+    @Nested
+    class GetTests {
+        @Test
+        void AllNotes_Ok() {
+            final int amountOfNotes = 5;
+            for (int i = 0; i < amountOfNotes; i++) {
+                noteRepository.save(new Note()).block();
+            }
 
-        webTestClient.delete().uri("/notes/" + note.getId() + "/")
-                .exchange()
-                .expectStatus().isOk();
+            webTestClient.get().uri(NOTES_BASE_URL)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBodyList(Note.class)
+                    .hasSize(amountOfNotes);
+        }
 
-        Assertions.assertNull(noteRepository.findById(note.getId()).block());
+        @Test
+        void ExistingId_Ok() {
+            Note testNote = noteRepository.save(new Note()).block();
+
+            webTestClient.get().uri(NOTES_BASE_URL + testNote.getId() + "/")
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(Note.class)
+                    .consumeWith(response -> {
+                        Note responseNote = response.getResponseBody();
+                        Assertions.assertEquals(testNote.getId(), responseNote.getId());
+                    });
+        }
+
+        @Test
+        void NotExistingId_NotFound() {
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.get().uri(NOTES_BASE_URL + randomString)
+                    .exchange()
+                    .expectStatus().isNotFound()
+                    .expectBody(NoteNotFoundException.class);
+        }
+    }
+
+    @Nested
+    class UpdateTests {
+        @Test
+        void UpdateName_ExistingId_Ok() {
+            Note testNote = noteRepository.save(new Note()).block();
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.put().uri(NOTES_BASE_URL + testNote.getId() + "/name/")
+                    .body(Mono.just(randomString), String.class)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(Note.class)
+                    .consumeWith(response -> {
+                        Note responseNote = response.getResponseBody();
+                        Assertions.assertEquals(randomString, responseNote.getName());
+                    });
+        }
+
+        @Test
+        void UpdateIcon_ExistingId_Ok() {
+            Note testNote = noteRepository.save(new Note()).block();
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.put().uri(NOTES_BASE_URL + testNote.getId() + "/icon/")
+                    .body(Mono.just(randomString), String.class)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(Note.class)
+                    .consumeWith(response -> {
+                        Note responseNote = response.getResponseBody();
+                        Assertions.assertEquals(randomString, responseNote.getIcon());
+                    });
+        }
+
+        @Test
+        void UpdateParent_ExistingId_Ok() {
+            Note testNote = noteRepository.save(new Note()).block();
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.put().uri(NOTES_BASE_URL + testNote.getId() + "/parent/")
+                    .body(Mono.just(randomString), String.class)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(Note.class)
+                    .consumeWith(response -> {
+                        Note responseNote = response.getResponseBody();
+                        Assertions.assertEquals(randomString, responseNote.getParent());
+                    });
+        }
+
+        @Test
+        void UpdateContent_ExistingId_Ok() {
+            Note testNote = noteRepository.save(new Note()).block();
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.put().uri(NOTES_BASE_URL + testNote.getId() + "/content/")
+                    .body(Mono.just(randomString), String.class)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(Note.class)
+                    .consumeWith(response -> {
+                        Note responseNote = response.getResponseBody();
+                        Assertions.assertEquals(randomString, responseNote.getContent());
+                    });
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"name", "parent", "icon", "content"})
+        void UpdateProperty_NotExistingId_NotFound(String parameterName) {
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.put().uri(NOTES_BASE_URL + randomString + "/" + parameterName)
+                    .exchange()
+                    .expectStatus().isNotFound()
+                    .expectBody(NoteNotFoundException.class);
+        }
+
+        @Test
+        void UpdateTags_Existing_Ok() {
+            Note testNote = noteRepository.save(new Note()).block();
+            List<Tag> tags = new ArrayList<>();
+            tags.add(new Tag("test", "test"));
+
+            webTestClient.put().uri(NOTES_BASE_URL + testNote.getId() + "/tags/")
+                    .body(Mono.just(tags), List.class)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(Note.class).consumeWith(response -> {
+                Note responseNote = response.getResponseBody();
+                Assertions.assertEquals(tags, responseNote.getTags());
+            });
+        }
+
+        @Test
+        void UpdateTags_NotExisting_NotFound() {
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+            List<Tag> tags = new ArrayList<>();
+            tags.add(new Tag("test", "test"));
+
+            webTestClient.put().uri(NOTES_BASE_URL + randomString + "/tags")
+                    .body(Mono.just(tags), List.class)
+                    .exchange()
+                    .expectStatus().isNotFound()
+                    .expectBody(NoteNotFoundException.class);
+        }
+    }
+
+    @Nested
+    class DeleteTests {
+        @Test
+        void ExistingId_Ok() {
+            Note note = noteRepository.save(new Note()).block();
+
+            webTestClient.delete().uri(NOTES_BASE_URL + note.getId() + "/")
+                    .exchange()
+                    .expectStatus().isOk();
+
+            Assertions.assertFalse(noteRepository.existsById(note.getId()).block());
+        }
+
+        @Test
+        void NotExistingId_NotFound() {
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+
+            webTestClient.delete().uri(NOTES_BASE_URL + randomString)
+                    .exchange()
+                    .expectStatus().isNotFound()
+                    .expectBody(NoteNotFoundException.class);
+        }
     }
 
     @Test
