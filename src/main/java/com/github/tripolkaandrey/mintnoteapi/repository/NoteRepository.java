5cf5ff38234ca021192a5dd823930749734c1a99
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-27 13:33:52 +0300

    add retrieving notes by user id

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/NoteRepository.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/NoteRepository.java
@@ -3,7 +3,9 @@ package com.github.tripolkaandrey.mintnoteapi.repository;
 import com.github.tripolkaandrey.mintnoteapi.entity.Note;
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface NoteRepository extends FirestoreReactiveRepository<Note> {
+    Flux<Note> findAllByUserId(String userId);
 }
