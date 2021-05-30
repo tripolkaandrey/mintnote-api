fb05451dffc6a1666a05c9a205e133c0d12b60df
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-30 12:36:47 +0300

    add repo method for checking if user has directory with specific path

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/DirectoriesRepository.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/DirectoriesRepository.java
@@ -2,13 +2,27 @@ package com.github.tripolkaandrey.mintnoteapi.repository;
 
 import com.github.tripolkaandrey.mintnoteapi.entity.Directories;
 import com.github.tripolkaandrey.mintnoteapi.entity.Directory;
+import com.github.tripolkaandrey.mintnoteapi.valueobject.Path;
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface DirectoriesRepository extends FirestoreReactiveRepository<Directories> {
     default Flux<Directory> findAllByUserId(String userId) {
         return this.findById(userId).flatMapIterable(Directories::getCollection);
+    }
+
+    default Mono<Boolean> existsByUserIdAndPath(String userId, Path path) {
+        if (path.equals(Directories.ROOT)) {
+            return Mono.just(true);
+        }
+
+        return this.findAllByUserId(userId)
+                .any(directory ->
+                        directory.getName().equals(path.getName()) &&
+                        directory.getParent().equals(path.getParent())
+                );
     }
 }
