19077456ecd14c0a961400d73ae5ee7cc682b7d5
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-30 12:36:07 +0300

    add repo method for retrieving all user directories

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/DirectoriesRepository.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/DirectoriesRepository.java
@@ -1,10 +1,14 @@
 package com.github.tripolkaandrey.mintnoteapi.repository;
 
 import com.github.tripolkaandrey.mintnoteapi.entity.Directories;
+import com.github.tripolkaandrey.mintnoteapi.entity.Directory;
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface DirectoriesRepository extends FirestoreReactiveRepository<Directories> {
-
+    default Flux<Directory> findAllByUserId(String userId) {
+        return this.findById(userId).flatMapIterable(Directories::getCollection);
+    }
 }
