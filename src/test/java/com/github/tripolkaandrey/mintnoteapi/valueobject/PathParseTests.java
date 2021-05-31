703e6e6fc3f5c0b302cc4f71dc622c342af9e446
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-31 13:14:22 +0300

    add unit tests for path parsing

---
+++ src/test/java/com/github/tripolkaandrey/mintnoteapi/valueobject/PathParseTests.java
@@ -0,0 +1,41 @@
+package com.github.tripolkaandrey.mintnoteapi.valueobject;
+
+import com.github.tripolkaandrey.mintnoteapi.entity.Directories;
+import com.github.tripolkaandrey.mintnoteapi.exception.InvalidPathException;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class PathTests {
+    @Nested
+    class Parse {
+        @Test
+        void Null_InvalidPathException() {
+            Assertions.assertThrows(InvalidPathException.class, () -> Path.parse(null));
+        }
+
+        @Test
+        void Separator_ReturnsRoot() {
+            Assertions.assertEquals(Directories.ROOT, Path.parse(Path.SEPARATOR));
+        }
+
+        @Test
+        void NoSeparatorPresent_InvalidPathException() {
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+            Assertions.assertThrows(InvalidPathException.class, () -> Path.parse(randomString));
+        }
+
+        @Test
+        void TrailingSeparator_InvalidPathException() {
+            String randomString = RandomStringUtils.randomAlphanumeric(10);
+            Assertions.assertThrows(InvalidPathException.class, () -> Path.parse(randomString + "/"));
+        }
+
+        @Test
+        void ValidInput_DoesNotThrowInvalidPathException() {
+            String randomString = RandomStringUtils.randomAlphanumeric(5) + "/" + RandomStringUtils.randomAlphanumeric(5);
+            Assertions.assertDoesNotThrow(() -> Path.parse(randomString));
+        }
+    }
+}
