555668cce3325afec3bb5b82b69f42257d7ca4ef
tripolkaandrey <andrii.trypilka@nure.ua>
2021-05-31 12:40:15 +0300

    add builder to note

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/entity/Note.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/entity/Note.java
@@ -4,6 +4,8 @@ import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.cloud.spring.data.firestore.Document;
 import com.google.common.base.Objects;
 
+import java.util.List;
+
 @Document(collectionName = "notes")
 public final class Note extends FileSystemUnit {
     @DocumentId
@@ -49,5 +51,52 @@ public final class Note extends FileSystemUnit {
     @Override
     public int hashCode() {
         return Objects.hashCode(super.hashCode(), id, content, userId);
+    }
+
+    public static class Builder {
+        private Note note;
+
+        public Builder() {
+            note = new Note();
+        }
+
+        public Builder reset() {
+            note = new Note();
+            return this;
+        }
+
+        public Builder withName(String name) {
+            note.setName(name);
+            return this;
+        }
+
+        public Builder withParent(String parent) {
+            note.setParent(parent);
+            return this;
+        }
+
+        public Builder withIcon(String icon) {
+            note.setIcon(icon);
+            return this;
+        }
+
+        public Builder withTags(List<Tag> tags) {
+            note.setTags(tags);
+            return this;
+        }
+
+        public Builder withContent(String content) {
+            note.setContent(content);
+            return this;
+        }
+
+        public Builder withUserId(String userId) {
+            note.setUserId(userId);
+            return this;
+        }
+
+        public Note build() {
+            return note;
+        }
     }
 }
