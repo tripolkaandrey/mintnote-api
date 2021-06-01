3ddf2547e137762b20af0bb153727267137677ce
tripolkaandrey <andrii.trypilka@nure.ua>
2021-06-01 11:02:31 +0300

    add repo method for retrieving all user tags

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/TagsRepository.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/TagsRepository.java
@@ -1,8 +1,13 @@
 package com.github.tripolkaandrey.mintnoteapi.repository;
 
+import com.github.tripolkaandrey.mintnoteapi.entity.Tag;
 import com.github.tripolkaandrey.mintnoteapi.entity.Tags;
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
+import reactor.core.publisher.Flux;
 
 public interface TagsRepository extends FirestoreReactiveRepository<Tags> {
+    default Flux<Tag> findAllByUserId(String userId) {
+        return this.findById(userId).flatMapIterable(Tags::getCollection);
+    }
 
 }
