37180489919eec04538d205c8b5e7e4de0705efd
tripolkaandrey <andrii.trypilka@nure.ua>
2021-06-01 11:08:54 +0300

    add repo method for checking tag existence

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/TagsRepository.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/repository/TagsRepository.java
@@ -4,10 +4,14 @@ import com.github.tripolkaandrey.mintnoteapi.entity.Tag;
 import com.github.tripolkaandrey.mintnoteapi.entity.Tags;
 import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface TagsRepository extends FirestoreReactiveRepository<Tags> {
     default Flux<Tag> findAllByUserId(String userId) {
         return this.findById(userId).flatMapIterable(Tags::getCollection);
     }
 
+    default Mono<Boolean> exists(String userId, Tag tag) {
+        return this.findById(userId).map(tags -> tags.getCollection().contains(tag));
+    }
 }
