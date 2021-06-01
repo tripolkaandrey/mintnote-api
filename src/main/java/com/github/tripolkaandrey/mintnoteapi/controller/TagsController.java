e56afb32a00f65b30d2d8e7485a951eb0956769a
tripolkaandrey <andrii.trypilka@nure.ua>
2021-06-01 11:12:12 +0300

    add endpoint for creating new tag

--- src/main/java/com/github/tripolkaandrey/mintnoteapi/controller/TagsController.java
+++ src/main/java/com/github/tripolkaandrey/mintnoteapi/controller/TagsController.java
@@ -1,12 +1,14 @@
 package com.github.tripolkaandrey.mintnoteapi.controller;
 
 import com.github.tripolkaandrey.mintnoteapi.entity.Tag;
+import com.github.tripolkaandrey.mintnoteapi.exception.TagAlreadyExistsException;
 import com.github.tripolkaandrey.mintnoteapi.repository.TagsRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
 import java.security.Principal;
 
 @RestController
@@ -21,5 +23,19 @@ public class TagsController {
     @GetMapping
     public Flux<Tag> getAll(Principal principal) {
         return tagsRepository.findAllByUserId(principal.getName());
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<Tag>> create(Principal principal, @RequestBody Tag tag) {
+        return tagsRepository
+                .exists(principal.getName(), tag)
+                .flatMap(exists -> {
+                    if (Boolean.TRUE.equals(exists)) {
+                        return Mono.error(TagAlreadyExistsException::new);
+                    }
+                    return Mono.empty();
+                })
+                .then(tagsRepository.add(principal.getName(), tag))
+                .map(t -> ResponseEntity.created(URI.create("/tags/")).body(t));
     }
 }
